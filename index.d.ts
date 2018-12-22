85f5ddd38f718ed21078b3281ae1c2884adf37c8
gaelph <contact@gael-philippe.fr>
2018-12-23 00:23:33 +0100

    declare the RouterReducerType

--- index.d.ts
+++ index.d.ts
@@ -22,7 +22,17 @@ declare namespace SimredReactRouter {
   export const parsePath: typeof ParsePath;
   export const createPath: typeof CreatePath;
   export const createConnectedRouter: (nameInState: string) => typeof ConnectedReactRouter
-  export const createRouterReducer: (history: History) => any
+  export const createRouterReducer: (history: History) => RouterReducerType
+
+  export type RouterReducerType = {
+    locationChange: (location: Location, action: string) => void
+    callHistoryMethod: (method: string, location: Location, action: string) => void
+    push: (location: Location, action: string) => void
+    replace: (location: Location, action: string) => void
+    go: (location: Location, action: string) => void
+    goBack: (location: Location, action: string) => void
+    goForward: (location: Location, action: string) => void
+  }
 }
 
 export = SimredReactRouter
