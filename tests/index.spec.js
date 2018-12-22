f1948716b84308e960d2805c409dbd10b55842a9
gaelph <contact@gael-philippe.fr>
2018-12-22 18:59:05 +0100

    use enzyme to test the router behaviour

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()})
+
 import { createConnectedRouter } from '../src/index'
 import { createRouterReducer } from '../src/index'
 import { createMemoryHistory } from '../src/index'
 
 import Simred from 'simred'
 
-import { renderToString } from 'react-dom/server'
 import { Provider } from 'react-simred'
 
 import { Route, Switch, Redirect } from 'react-router'
@@ -29,7 +32,7 @@ describe('Connected Router', function () {
 
       const ConnectedRouter = createConnectedRouter('router')
 
-      const res = renderToString((
+      const res = mount((
         <Provider store={store}>
           <ConnectedRouter history={history}>
             <Route path="/" render={props => <div>rendered</div>} />
@@ -37,8 +40,7 @@ describe('Connected Router', function () {
         </Provider>
       ))
 
-      console.log(res)
-      console.log(store.getState().router)
+      expect(res.html()).toEqual("<div>rendered</div>")
     }
 
     expect(render).not.toThrow()
@@ -62,7 +64,7 @@ describe('Connected Router', function () {
 
       const ConnectedRouter = createConnectedRouter('router')
 
-      const res = renderToString((
+      const res = mount((
         <Provider store={store}>
           <ConnectedRouter history={history}>
             <Switch>
@@ -73,11 +75,105 @@ describe('Connected Router', function () {
         </Provider>
       ))
 
-      console.log(res)
-      console.log(store.getState().router)
+      const pathname = store.getState().router.location.pathname
+      const render = res.html()
+
+      expect(pathname).toEqual("/other")
+      expect(render).toEqual("<div>other</div>")
     }
 
     expect(render).not.toThrow()
+  })
 
+  it('pushes', function () {
+    const render = () => {
+      const history = createMemoryHistory({
+        initialEntries: ['/'], // The initial URLs in the history stack
+        initialIndex: 0, // The starting index in the history stack
+        keyLength: 6, // The length of location.key
+        // A function to use to confirm navigation with the user. Required
+        // if you return string prompts from transition hooks (see below)
+        getUserConfirmation: null
+      })
+
+      const store = Simred.createStore({
+        router: createRouterReducer(history)
+      })
+
+      const ConnectedRouter = createConnectedRouter('router')
+
+      const res = mount((
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route exact path="/other" render={props => <div>other</div>} />
+              <Route exact path="/" render={props => <div>slash</div>} />
+            </Switch>
+          </ConnectedRouter>
+        </Provider>
+      ))
+
+      const pathname = store.getState().router.location.pathname
+      expect(res.containsMatchingElement(<div>slash</div>))
+      // const render = res.html()
+
+      expect(pathname).toEqual("/")
+      // expect(render).toEqual("<div>slash</div>")
+
+      store.getActions().router.push('/other')
+      console.log(res.containsMatchingElement(<div>other</div>))
+    }
+
+    expect(render).not.toThrow()
+  })
+
+  it('goes back', function () {
+    const render = () => {
+      const history = createMemoryHistory({
+        initialEntries: ['/'], // The initial URLs in the history stack
+        initialIndex: 0, // The starting index in the history stack
+        keyLength: 6, // The length of location.key
+        // A function to use to confirm navigation with the user. Required
+        // if you return string prompts from transition hooks (see below)
+        getUserConfirmation: null
+      })
+
+      const store = Simred.createStore({
+        router: createRouterReducer(history)
+      })
+
+      const ConnectedRouter = createConnectedRouter('router')
+
+      const res = mount((
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route exact path="/other" render={props => <div>other</div>} />
+              <Route exact path="/" render={props => <div>slash</div>} />
+            </Switch>
+          </ConnectedRouter>
+        </Provider>
+      ))
+
+      const pathname = store.getState().router.location.pathname
+      expect(res.containsMatchingElement(<div>slash</div>)).toBeTruthy()
+      console.log(res.html())
+
+      expect(pathname).toEqual("/")
+      // expect(render).toEqual("<div>slash</div>")
+
+      const routerActions = store.getActions().router
+
+      routerActions.push('/other')
+      routerActions.goBack()
+      routerActions.goForward()
+      routerActions.go(0)
+      routerActions.replace('/other')
+      console.log(res.html())
+
+      res.unmount()
+    }
+
+    expect(render).not.toThrow()
   })
 })
